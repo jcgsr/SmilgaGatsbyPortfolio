af7d79dfc0c0e9357ce811db970abe4d6bc64ecc
jcgsr <cabralguerra@gmail.com>
2021-07-04 20:44:32 -0300

    projetos

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,46 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Seo from "../components/Seo"
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const {
+    allStrapiProject: { nodes: projects }, // alias
+  } = data
   return (
     <>
       <main>
         <Hero />
         <Services />
         <Jobs />
+        <Projects title="projetos em destaque" showLink projects={projects} />
       </main>
     </>
   )
 }
-
+export const query = graphql`
+  {
+    allStrapiProject(filter: { featured: { eq: true } }) {
+      nodes {
+        description
+        featured
+        github
+        id
+        slug
+        title
+        url
+        stack {
+          id
+          title
+        }
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+            }
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`
 export default IndexPage
